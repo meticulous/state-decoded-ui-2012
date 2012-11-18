0e11300f427b5d069209ca7230b66baa13560fa0
John Athayde <john.athayde@livingsocial.com>
2012-11-18 14:43:15 -0500

    Setting up Compass/Sass

---
+++ config.rb
@@ -0,0 +1,27 @@
+# DO NOT DELETE - This is used for CodeKit to compile Sass for non asset pipeline projects, like this one.
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images/"
+javascripts_dir = "javascripts/"
+fonts_dir ="fonts"
+
+output_style = :nested
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
