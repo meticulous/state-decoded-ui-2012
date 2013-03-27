8b838160e162b84e1b7696109aee1334967bfbb0
Lynn Wallenstein <lynn.wallenstein@gmail.com>
2013-03-27 12:38:47 -0400

    Keyboard Shortcut Modal and General CSS Cleanup

---
+++ public/js/ready.js
@@ -0,0 +1,37 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+
+jQuery(function($) {
+
+  console.log("Ready Loaded");
+  // Remove Preload so that transitions work after page loads
+  $("body").removeClass("preload");
+
+    Mousetrap.bind('?', function() {
+     //console.log("Question Mark Pressed");
+     $('#keyboard-shortcuts').modal("show");
+    });
+
+  // Handler for .ready() called.
+});
